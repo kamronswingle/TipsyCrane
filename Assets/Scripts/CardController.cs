d27563c96b20162085b836c9c2f468b3b6b91aed
Jeffrey Kraskouskas <kraskouskas@chapman.edu>
2025-05-13 16:43:44 -0700

    MatchingGameV1
    Allows you to match three pairs of cards. They are randomized. Has tweening animations.

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PrimeTween;
+using UnityEditor;
+
+public class CardController : MonoBehaviour
+{
+    [SerializeField] private Card cardPrefab;
+    [SerializeField] private Transform gridTransform;
+    [SerializeField] private Sprite[] sprites;
+
+    private List<Sprite> spritePairs;
+    private Card firstSelected;
+    private Card secondSelected;
+
+    private int matchCounts;
+
+    private void Start()
+    {
+        PrepareSprites();
+        CreateCards();
+    }
+
+    private void PrepareSprites()
+    {
+        spritePairs = new List<Sprite>();
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            spritePairs.Add(sprites[i]);
+            spritePairs.Add(sprites[i]);
+        }
+
+        ShuffleSprites(spritePairs);
+    }
+
+    void ShuffleSprites(List<Sprite> spriteList)
+    {
+        for (int i = spriteList.Count - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+
+            Sprite temp = spriteList[i];
+            spriteList[i] = spriteList[randomIndex];
+            spriteList[randomIndex] = temp;
+        }
+    }
+    
+    void CreateCards() {
+        for (int i = 0; i < spritePairs.Count; i++)
+        {
+            Card card = Instantiate(cardPrefab, gridTransform);
+            card.SetIconSprite(spritePairs[i]);
+            card.controller = this;
+        }
+    }
+
+    public void SetSelected(Card card)
+    {
+        if (card.isSelected == false)
+        {
+            card.Show();
+
+            if (firstSelected == null)
+            {
+                firstSelected = card;
+                return;
+            }
+
+            if (secondSelected == null)
+            {
+                secondSelected = card;
+                StartCoroutine(CheckMatching(firstSelected, secondSelected));
+                firstSelected = null;
+                secondSelected = null;
+            }
+        }
+    }
+
+    IEnumerator CheckMatching(Card a, Card b)
+    {
+        yield return new WaitForSeconds(0.3f);
+        if (a.iconSprite == b.iconSprite)
+        {
+            matchCounts++;
+            if (matchCounts >= spritePairs.Count / 2)
+            {
+                PrimeTween.Sequence.Create()
+                    .Chain(PrimeTween.Tween.Scale(gridTransform, Vector3.one * 1.2f, 0.2f,
+                        ease: PrimeTween.Ease.OutBack)).Chain(PrimeTween.Tween.Scale(gridTransform, Vector3.one, 0.1f));
+            }
+        }
+        else
+        {
+            a.Hide();
+            b.Hide();
+        }
+    }
+    
+}
